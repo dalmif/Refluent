7983bb6b0786aff5990db18ad3910d7d1971aca9
Mohammad Fallah <mohammad@shape.dk>
2025-12-10 19:37:49 +0100

    Integrate Room plugin

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -2,10 +2,15 @@ plugins {
     alias(libs.plugins.dalmif.android.library)
     alias(libs.plugins.dalmif.android.hilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 android {
     namespace = "io.kayt.refluent.core.ui"
+
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
