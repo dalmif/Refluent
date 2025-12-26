7c9405801055fbdd0d3f35be1fca7ddf31bf0041
Mohammad Fallah <mohammad@shape.dk>
2025-12-26 19:54:09 +0100

    Add some new color to the pallet

--- core/ui/src/main/java/io/kayt/refluent/core/ui/theme/AppTheme.kt
+++ core/ui/src/main/java/io/kayt/refluent/core/ui/theme/AppTheme.kt
@@ -1,14 +1,18 @@
 package io.kayt.refluent.core.ui.theme
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.LocalTonalElevationEnabled
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import io.kayt.refluent.core.ui.theme.colors.AppColors
+import io.kayt.refluent.core.ui.theme.colors.AppThemeDarkColors
 import io.kayt.refluent.core.ui.theme.colors.AppThemeLightColors
 import io.kayt.refluent.core.ui.theme.colors.LocalAppColors
+import io.kayt.refluent.core.ui.theme.colors.LocalDarkUi
+import io.kayt.refluent.core.ui.theme.colors.MaterialThemeDarkColors
 import io.kayt.refluent.core.ui.theme.colors.MaterialThemeLightColors
 import io.kayt.refluent.core.ui.theme.typography.AppTypography
 import io.kayt.refluent.core.ui.theme.typography.LocalAppTypography
@@ -22,23 +26,27 @@ object AppTheme {
     val typography: AppTypography
         @Composable
         get() = LocalAppTypography.current
+
+    val isDark: Boolean
+        @Composable
+        get() = LocalDarkUi.current
 }
 
 @SuppressLint("ComposeModifierMissing")
 @Composable
 fun AppTheme(
-    content: @Composable () -> Unit,
+    isDark: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
 ) {
-    val materialColors = MaterialThemeLightColors
-    val appColors = AppThemeLightColors
 
     CompositionLocalProvider(
-        LocalAppColors provides appColors,
+        LocalAppColors provides if (!isDark) AppThemeLightColors else AppThemeDarkColors,
         LocalAppTypography provides appTypography,
-        LocalTonalElevationEnabled provides false
+        LocalTonalElevationEnabled provides false,
+        LocalDarkUi provides isDark
     ) {
         MaterialTheme(
-            colorScheme = materialColors,
+            colorScheme = if (!isDark) MaterialThemeLightColors else MaterialThemeDarkColors,
             content = {
                 Surface(content = content)
             }
